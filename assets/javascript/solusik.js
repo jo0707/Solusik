23dff80c5bd9dae79be4563ee5a233a8c56485a9
Josua Sinaga <josua123690707@gmail.com>
2020-08-08 17:14:42 +0700

    adding click support to image

--- assets/javascript/solusik.js
+++ assets/javascript/solusik.js
@@ -1,6 +1,16 @@
-let name            = prompt("Siapakah nama anda?")
-let greetingText    = document.getElementById("greetingText")
+let name            = prompt("Siapakah nama anda?");
+let greetingText    = document.getElementById("greetingText");
+let images          = document.body.querySelectorAll(".flex-art-image");
+
+for (let i=0;i<images.length;i++) {
+    with (images[i]) {
+        addEventListener('click', function(event) {
+        window.open(getAttribute("src"), "_blank");
+        })
+    }
+}
 
 if (name != null) 
-    greetingText.innerText  = "Selamat Datang, " + name + "!"
-else greetingText.innerText  = "Selamat Datang!"
+    greetingText.innerText  = "Selamat Datang, " + name + "!";
+else 
+    greetingText.innerText  = "Selamat Datang!";
